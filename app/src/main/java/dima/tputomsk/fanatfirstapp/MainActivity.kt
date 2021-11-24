20b905735a5d6982e6746c6f247ae81c86ef3e48
Fanatf1 <fanatf1@gmail.com>
2021-11-24 23:53:22 +0700

    first commit

---
+++ app/src/main/java/dima/tputomsk/fanatfirstapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package dima.tputomsk.fanatfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+авававафцу 3ук23квцуйвйц
+
+    }
+}
